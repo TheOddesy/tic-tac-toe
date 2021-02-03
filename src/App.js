024783f8cef558c645cb4e6484b1d17917f81c40
Nicolas Salas <nicolassalas@Nicolass-MBP.lan>
2021-02-03 16:52:31 +0100

    hej

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Game from './components/Game'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="big-circle-1"></div>
+      <div className="moon"></div>
+      <div className="triangle"></div>
+      <div className="info"><a href="https://theoddesy.github.io/HomePage/" target="_blank" rel="noopener noreferrer" >Who made this?</a></div>
+      
+      <Game />
+    </>
   );
 }
 
